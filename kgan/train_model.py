1d42f4f0df2b3a9eeab5b6bb943a78dfd9c483d3
Mr. Pritam Prakash Shete <pritam.shete@gmail.com>
2020-07-22 17:30:46 +0530

    kgan is added.

--- kgan/train_model.py
+++ kgan/train_model.py
@@ -15,7 +15,7 @@ dataset = DatasetFactory.create('mnist')
 train_dataset, validation_dataset = dataset.load(input_shape, batch_size)
 
 epochs = 10
-learning_rate = 0.0002
+learning_rate = 0.0001
 status = gan.train(train_dataset, batch_size, epochs, learning_rate,
                    validation_dataset)
 print(status)
