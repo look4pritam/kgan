4af1594ea24928de45b0647f725d9f20fe94e9e4
Mr. Pritam Prakash Shete <pritam.shete@gmail.com>
2020-07-22 12:18:49 +0530

    kgan is added.

--- kgan/models/GAN.py
+++ kgan/models/GAN.py
@@ -2,7 +2,24 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import tensorflow as tf
+import tensorflow.keras.layers as layers
+import tensorflow.keras.models as models
+
 
 def create_discriminator(input_shape):
-    discriminator = None
+    discriminator = models.Sequential(name='discriminator')
+
+    discriminator.add(layers.Flatten(input_shape=input_shape))
+
+    discriminator.add(layers.Dense(units=1024))
+    discriminator.add(layers.LeakyReLU(alpha=0.2))
+
+    discriminator.add(layers.Dense(units=512))
+    discriminator.add(layers.LeakyReLU(alpha=0.2))
+
+    discriminator.add(layers.Dense(units=256))
+    discriminator.add(layers.LeakyReLU(alpha=0.2))
+
+    discriminator.add(layers.Dense(units=1, activation='sigmoid'))
     return (discriminator)
