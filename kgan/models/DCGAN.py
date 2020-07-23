56a65879f5ba5193c6853dc0c45c3c304104b094
Mr. Pritam Prakash Shete <pritam.shete@gmail.com>
2020-07-23 16:23:55 +0530

    DCGAN is added.

--- kgan/models/DCGAN.py
+++ kgan/models/DCGAN.py
@@ -17,10 +17,10 @@ cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 class DCGAN(AbstractGAN):
     @classmethod
     def name(cls):
-        return ('gan')
+        return ('dcgan')
 
     def __init__(self, input_shape, latent_dimension):
-        super(SimpleGAN, self).__init__()
+        super(DCGAN, self).__init__()
 
         self._input_shape = input_shape
         self._latent_dimension = latent_dimension
@@ -52,7 +52,7 @@ class DCGAN(AbstractGAN):
             tf.ones_like(real_predictions), real_predictions)
         fake_loss = cross_entropy(
             tf.zeros_like(fake_predictions), fake_predictions)
-        discriminator_loss = 0.5 * (real_loss + fake_loss)
+        discriminator_loss = real_loss + fake_loss
         return (discriminator_loss)
 
     def generate(self, number_of_samples):
@@ -61,7 +61,7 @@ class DCGAN(AbstractGAN):
         generated_images = self._generator.predict(generator_inputs)
         generated_images = generated_images.reshape(
             number_of_samples, self._input_shape[0], self._input_shape[1])
-        generated_images = ((generated_images + 1.) / 2.) * 255.
+        generated_images = generated_images * 127.5 + 127.5
         return (generated_images)
 
     def save_generated(self, number_of_samples=10):
@@ -74,63 +74,6 @@ class DCGAN(AbstractGAN):
         for key_value, loss_value in losses.items():
             print(key_value, '-', loss_value.numpy())
 
-    def _update_discriminator(self, input_batch):
-        real_samples = input_batch
-        generator_inputs = tf.random.normal(
-            [self.batch_size(), self.latent_dimension()])
-
-        self._generator.trainable = False
-        self._discriminator.trainable = True
-        with tf.GradientTape(persistent=True) as tape:
-            fake_samples = self._generator(generator_inputs, training=False)
-
-            real_predictions = self._discriminator(real_samples, training=True)
-            fake_predictions = self._discriminator(fake_samples, training=True)
-
-            discriminator_loss = self._discriminator_loss(
-                real_predictions, fake_predictions)
-
-        discriminator_loss_gradients = tape.gradient(
-            target=discriminator_loss,
-            sources=self._discriminator.trainable_variables)
-
-        self._discriminator_optimizer.apply_gradients(
-            zip(discriminator_loss_gradients,
-                self._discriminator.trainable_variables))
-
-        return (discriminator_loss)
-
-    def _update_generator(self, input_batch):
-        generator_inputs = tf.random.normal(
-            [self.batch_size() * 2,
-             self.latent_dimension()])
-
-        self._generator.trainable = True
-        self._discriminator.trainable = False
-        with tf.GradientTape(persistent=True) as tape:
-            fake_samples = self._generator(generator_inputs, training=True)
-            fake_predictions = self._discriminator(
-                fake_samples, training=False)
-            generator_loss = self._generator_loss(fake_predictions)
-
-        generator_loss_gradients = tape.gradient(
-            target=generator_loss, sources=self._generator.trainable_variables)
-
-        self._generator_optimizer.apply_gradients(
-            zip(generator_loss_gradients, self._generator.trainable_variables))
-
-        return (generator_loss)
-
-    def _train_on_batch(self, input_batch):
-        discriminator_loss = self._update_discriminator(input_batch)
-        generator_loss = self._update_generator(input_batch)
-
-        return {
-            'generator': generator_loss,
-            'discriminator': discriminator_loss
-        }
-
-    '''
     def _train_on_batch(self, input_batch):
         real_samples = input_batch
         generator_inputs = tf.random.normal(
@@ -163,4 +106,3 @@ class DCGAN(AbstractGAN):
             'generator': generator_loss,
             'discriminator': discriminator_loss
         }
-    '''
