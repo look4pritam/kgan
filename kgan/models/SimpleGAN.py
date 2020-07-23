b2b5416c590beb296f2fcd26a86bc70646dc8b47
Mr. Pritam Prakash Shete <pritam.shete@gmail.com>
2020-07-23 14:43:34 +0530

    kgan is added.

--- kgan/models/SimpleGAN.py
+++ kgan/models/SimpleGAN.py
@@ -80,8 +80,10 @@ class SimpleGAN(AbstractGAN):
         generator_inputs = tf.random.normal(
             [self.batch_size(), self.latent_dimension()])
 
+        self._generator.trainable = False
+        self._discriminator.trainable = True
         with tf.GradientTape(persistent=True) as tape:
-            fake_samples = self._generator.predict(generator_inputs)
+            fake_samples = self._generator(generator_inputs, training=False)
 
             real_predictions = self._discriminator(real_samples, training=True)
             fake_predictions = self._discriminator(fake_samples, training=True)
@@ -104,9 +106,12 @@ class SimpleGAN(AbstractGAN):
             [self.batch_size() * 2,
              self.latent_dimension()])
 
+        self._generator.trainable = True
+        self._discriminator.trainable = False
         with tf.GradientTape(persistent=True) as tape:
             fake_samples = self._generator(generator_inputs, training=True)
-            fake_predictions = self._discriminator.predict(fake_samples)
+            fake_predictions = self._discriminator(
+                fake_samples, training=False)
             generator_loss = self._generator_loss(fake_predictions)
 
         generator_loss_gradients = tape.gradient(
