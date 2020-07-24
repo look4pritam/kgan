9675a504ea28a4f410424ddd3bc5e6991fe12a34
Mr. Pritam Prakash Shete <pritam.shete@gmail.com>
2020-07-24 18:22:55 +0530

    Batch normalization layer is added.

--- kgan/layers/BatchNormalization.py
+++ kgan/layers/BatchNormalization.py
@@ -2,13 +2,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow.keras.layers as layers
+import tensorflow.keras.layers as klayers
 
 
-class BatchNormalization(layers.Layer):
+class BatchNormalization(klayers.Layer):
     def __init__(self, is_training=False):
         super(BatchNormalization, self).__init__()
-        self.bn = layers.BatchNormalization(
+        self.bn = klayers.BatchNormalization(
             epsilon=1e-5, momentum=0.9, scale=True, trainable=is_training)
 
     def call(self, inputs, training):
