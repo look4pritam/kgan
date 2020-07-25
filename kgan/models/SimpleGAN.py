a462620b87087a765b5d996d9a8f79560aec38b8
Mr. Pritam Prakash Shete <pritam.shete@gmail.com>
2020-07-25 11:11:16 +0530

    WGAN-GP model is added.

--- kgan/models/SimpleGAN.py
+++ kgan/models/SimpleGAN.py
@@ -2,28 +2,21 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from kgan.models.AbstractGAN import AbstractGAN
+from kgan.models.ImageGAN import ImageGAN
 
 from kgan.models.discriminators.SimpleDiscriminator import SimpleDiscriminator
 from kgan.models.generators.SimpleGenerator import SimpleGenerator
 
 import tensorflow as tf
 
-import cv2
 
-cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-
-
-class SimpleGAN(AbstractGAN):
+class SimpleGAN(ImageGAN):
     @classmethod
     def name(cls):
         return ('gan')
 
     def __init__(self, input_shape, latent_dimension):
         super(SimpleGAN, self).__init__()
-
-        self._input_shape = input_shape
-        self._latent_dimension = latent_dimension
 
         self._discriminator = self._create_discriminator()
         self._generator = self._create_generator()
@@ -36,44 +29,6 @@ class SimpleGAN(AbstractGAN):
         generator = SimpleGenerator.create(self.input_shape(),
                                            self.latent_dimension())
         return (generator)
-
-    def input_shape(self):
-        return (self._input_shape)
-
-    def latent_dimension(self):
-        return (self._latent_dimension)
-
-    def _generator_loss(self, fake_predictions):
-        generator_loss = cross_entropy(
-            tf.ones_like(fake_predictions), fake_predictions)
-        return (generator_loss)
-
-    def _discriminator_loss(self, real_predictions, fake_predictions):
-        real_loss = cross_entropy(
-            tf.ones_like(real_predictions), real_predictions)
-        fake_loss = cross_entropy(
-            tf.zeros_like(fake_predictions), fake_predictions)
-        discriminator_loss = 0.5 * (real_loss + fake_loss)
-        return (discriminator_loss)
-
-    def generate(self, number_of_samples):
-        generator_inputs = tf.random.normal(
-            [number_of_samples, self.latent_dimension()])
-        generated_images = self._generator.predict(generator_inputs)
-        generated_images = generated_images.reshape(
-            number_of_samples, self._input_shape[0], self._input_shape[1])
-        generated_images = ((generated_images + 1.) / 2.) * 255.
-        return (generated_images)
-
-    def save_generated(self, number_of_samples=10):
-        generated_images = self.generate(number_of_samples)
-        for index, image in enumerate(generated_images):
-            filename = 'image-' + str(index) + '.png'
-            cv2.imwrite(filename, image)
-
-    def _print_losses(self, losses):
-        for key_value, loss_value in losses.items():
-            print(key_value, '-', loss_value.numpy())
 
     def _update_discriminator(self, input_batch):
         real_samples = input_batch
