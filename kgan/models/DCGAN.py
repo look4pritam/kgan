37c81a5d357164e040f765b3e0e277f6aaba5ee4
Mr. Pritam Prakash Shete <pritam.shete@gmail.com>
2020-07-27 11:30:17 +0530

    Dataset augmentation is added.

--- kgan/models/DCGAN.py
+++ kgan/models/DCGAN.py
@@ -10,6 +10,7 @@ from kgan.models.generators.ConvolutionalGenerator import ConvolutionalGenerator
 import tensorflow as tf
 cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
+
 class DCGAN(ImageGAN):
     @classmethod
     def name(cls):
@@ -40,7 +41,7 @@ class DCGAN(ImageGAN):
 
         # Sample random points in the latent space.
         generator_inputs = tf.random.normal(
-            shape=(self.batch_size(), self.latent_dimension()) )
+            shape=(self.batch_size(), self.latent_dimension()))
 
         # Decode these random points to fake images.
         generated_images = self._generator(generator_inputs)
@@ -49,39 +50,46 @@ class DCGAN(ImageGAN):
         combined_images = tf.concat([generated_images, real_images], axis=0)
 
         # Create combined labels for discriminating real images from generated images.
-        labels = tf.concat([tf.ones((self.batch_size(), 1)),  tf.zeros((self.batch_size(), 1))], axis=0)
+        labels = tf.concat([
+            tf.ones((self.batch_size(), 1)),
+            tf.zeros((self.batch_size(), 1))
+        ],
+                           axis=0)
 
         # Add random noise to the labels. (important trick)
         labels += 0.05 * tf.random.uniform(tf.shape(labels))
 
         # Train the discriminator.
         with tf.GradientTape() as tape:
-           predictions = self._discriminator(combined_images)
-           discriminator_loss = cross_entropy(labels, predictions)
+            predictions = self._discriminator(combined_images)
+            discriminator_loss = cross_entropy(labels, predictions)
 
-        gradients = tape.gradient(discriminator_loss, self._discriminator.trainable_weights)
-        self._discriminator_optimizer.apply_gradients(zip(gradients, self._discriminator.trainable_weights))
+        gradients = tape.gradient(discriminator_loss,
+                                  self._discriminator.trainable_weights)
+        self._discriminator_optimizer.apply_gradients(
+            zip(gradients, self._discriminator.trainable_weights))
 
         # Sample random points in the latent space.
         generator_inputs = tf.random.normal(
-            shape=(self.batch_size(), self.latent_dimension()) )
+            shape=(self.batch_size(), self.latent_dimension()))
 
         # Create misleading labels, which will be predicted as real images.
         misleading_labels = tf.zeros((self.batch_size(), 1))
 
         # Train the generator.
         with tf.GradientTape() as tape:
-           # Decode random point to fake images.
-           generated_images = self._generator(generator_inputs)
-          
-           predictions = self._discriminator(generated_images)
-           generator_loss = cross_entropy(misleading_labels, predictions)
+            # Decode random point to fake images.
+            generated_images = self._generator(generator_inputs)
 
-        gradients = tape.gradient(generator_loss, self._generator.trainable_weights)
-        self._generator_optimizer.apply_gradients(zip(gradients, self._generator.trainable_weights)
+            predictions = self._discriminator(generated_images)
+            generator_loss = cross_entropy(misleading_labels, predictions)
+
+        gradients = tape.gradient(generator_loss,
+                                  self._generator.trainable_weights)
+        self._generator_optimizer.apply_gradients(
+            zip(gradients, self._generator.trainable_weights))
 
         return {
             'generator': generator_loss,
             'discriminator': discriminator_loss
         }
-
