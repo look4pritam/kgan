463799c51ae23a097c27993e39f446a82e767f20
Mr. Pritam Prakash Shete <pritam.shete@gmail.com>
2020-07-30 12:24:26 +0530

    AttGAN is added.

---
+++ kgan/models/AttGAN.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from kgan.models.WGANGP import WGANGP
+
+from kgan.models.discriminators.ConvolutionalDiscriminator import ConvolutionalDiscriminator
+from kgan.models.generators.ConvolutionalGenerator import ConvolutionalGenerator
+
+import tensorflow as tf
+
+
+class AttGAN(WGANGP):
+    @classmethod
+    def name(cls):
+        return ('attgan')
+
+    def __init__(self, input_shape, latent_dimension):
+        super(AttGAN, self).__init__(input_shape, latent_dimension)
+        pass
+
+    def _create_discriminator(self):
+        discriminator = ConvolutionalDiscriminator.create(self.input_shape())
+        return (discriminator)
+
+    def _create_generator(self):
+        generator = ConvolutionalGenerator.create(self.latent_dimension())
+        return (generator)
