e0b0bf183a40ddc52fe4aaee1dab29da2fa3ae28
Mr. Pritam Prakash Shete <pritam.shete@gmail.com>
2020-07-30 12:54:34 +0530

    AttGAN is added.

--- kgan/models/AbstractGAN.py
+++ kgan/models/AbstractGAN.py
@@ -160,8 +160,7 @@ class AbstractGAN(object):
         status = True
 
         # Preprocess the dataset.
-        train_dataset, number_of_batches = self._preprocess_dataset(
-            dataset, batch_size)
+        #train_dataset, number_of_batches = self._preprocess_dataset(dataset, batch_size)
 
         # Set parameters used for model training.
         self.set_learning_rate(learning_rate)
