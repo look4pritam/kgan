41002f38850232bd489e4dd53520268ff27e4ed0
Mr. Pritam Prakash Shete <pritam.shete@gmail.com>
2020-07-30 16:03:02 +0530

    AttGAN is added.

--- kgan/models/generators/AttGANEncoder.py
+++ kgan/models/generators/AttGANEncoder.py
@@ -11,7 +11,6 @@ from functools import partial
 
 
 class AttGANEncoder(models.Model):
-
     @classmethod
     def create(cls, input_shape, encoder_dimension=64, downsamplings_layers=5):
 
@@ -24,58 +23,66 @@ class AttGANEncoder(models.Model):
 
         return (encoder)
 
-  def __init__(self, encoder_dimension=64, downsamplings_layers=5, 
-               name='attgan-encoder', **kwargs):
-    super(Encoder, self).__init__(name=name, **kwargs)
-    
-    self._encoder_dimension = encoder_dimension
-    self._downsamplings_layers = downsamplings_layers
+    def __init__(self,
+                 encoder_dimension=64,
+                 downsamplings_layers=5,
+                 name='attgan-encoder',
+                 **kwargs):
+        super(Encoder, self).__init__(name=name, **kwargs)
 
-    self._encoders = []    
-    filters = self._encoder_dimension  
-    for block_index in range(self._downsamplings_layers):
-      block_name = 'block-' + str(block_index + 1)
+        self._encoder_dimension = encoder_dimension
+        self._downsamplings_layers = downsamplings_layers
 
-      current_encoder = self._convolution_block(filters, 4, name=block_name)
-      self._encoders.append(current_encoder)
+        self._encoders = []
+        filters = self._encoder_dimension
+        for block_index in range(self._downsamplings_layers):
+            block_name = 'block-' + str(block_index + 1)
 
-      filters = filters * 2
+            current_encoder = self._convolution_block(
+                filters, 4, name=block_name)
+            self._encoders.append(current_encoder)
 
-  def _convolution_block(self, filters, kernel_size, 
-                         activation_fn=tf.nn.leaky_relu, batch_norm=True, 
-                         input_shape=None, name=None):
-    
-    if( input_shape is None ):
-      conv = partial(layers.Conv2D)
-    else:
-      conv = partial(layers.Conv2D, input_shape=input_shape)
+            filters = filters * 2
 
-    blocks = [
-      conv(filters, 
-           (kernel_size, kernel_size), 
-           strides = (2,2), 
-           padding="same",            
-           use_bias = True,            
-           name='conv')
+    def _convolution_block(self,
+                           filters,
+                           kernel_size,
+                           activation_fn=tf.nn.leaky_relu,
+                           batch_norm=True,
+                           input_shape=None,
+                           name=None):
+
+        if (input_shape is None):
+            conv = partial(layers.Conv2D)
+        else:
+            conv = partial(layers.Conv2D, input_shape=input_shape)
+
+        blocks = [
+            conv(
+                filters, (kernel_size, kernel_size),
+                strides=(2, 2),
+                padding="same",
+                use_bias=True,
+                name='conv')
         ]
 
-    if( batch_norm ):
-      blocks.append(layers.BatchNormalization(name='bnorm'))
+        if (batch_norm):
+            blocks.append(layers.BatchNormalization(name='bnorm'))
 
-    if(activation_fn is not None):
-      if(activation_fn == tf.nn.leaky_relu):        
-        blocks.append(layers.LeakyReLU(alpha=0.2, name='act'))
-      else:
-        blocks.append(layers.Activation(activation_fn, name='act'))
+        if (activation_fn is not None):
+            if (activation_fn == tf.nn.leaky_relu):
+                blocks.append(layers.LeakyReLU(alpha=0.2, name='act'))
+            else:
+                blocks.append(layers.Activation(activation_fn, name='act'))
 
-    return(models.Sequential(blocks, name=name))
+        return (models.Sequential(blocks, name=name))
 
-  def call(self, input_image, training=True):
-    image_features = []
+    def call(self, input_image, training=True):
+        image_features = []
 
-    layer_input = input_image
-    for current_encoder in self._encoders:
-      layer_input = current_encoder(layer_input, training=training)
-      image_features.append(layer_input)
+        layer_input = input_image
+        for current_encoder in self._encoders:
+            layer_input = current_encoder(layer_input, training=training)
+            image_features.append(layer_input)
 
-    return(image_features)
+        return (image_features)
