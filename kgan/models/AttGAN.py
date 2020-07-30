4c81ce7f3fd59cc20ca5c82f8d8504b5a6fd04f7
Mr. Pritam Prakash Shete <pritam.shete@gmail.com>
2020-07-30 15:21:54 +0530

    AttGAN is added.

--- kgan/models/AttGAN.py
+++ kgan/models/AttGAN.py
@@ -7,6 +7,9 @@ from kgan.models.WGANGP import WGANGP
 from kgan.models.discriminators.AttGANDiscriminator import AttGANDiscriminator
 from kgan.models.generators.ConvolutionalGenerator import ConvolutionalGenerator
 
+import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
+
 
 class AttGAN(WGANGP):
     @classmethod
@@ -28,6 +31,17 @@ class AttGAN(WGANGP):
 
     def _normalize_dataset(self, image, attributes):
         return (image, attributes)
+
+    def _update_learning_rate(self, current_epoch, number_of_epochs):
+        return (True)
+
+    def _create_generator_optimizer(self, learning_rate):
+        optimizer = Adam(learning_rate=learning_rate, beta_1=0.5, beta_2=0.999)
+        return (optimizer)
+
+    def _create_discriminator_optimizer(self, learning_rate):
+        optimizer = Adam(learning_rate=learning_rate, beta_1=0.5, beta_2=0.999)
+        return (optimizer)
 
     def _train_on_batch(self, input_batch):
 
