ef5d6df57893b84aa2024543aeaeb48271fe0eef
Mr. Pritam Prakash Shete <pritam.shete@gmail.com>
2020-07-30 14:05:46 +0530

    CelebA dataset is added.

--- kgan/datasets/CelebA.py
+++ kgan/datasets/CelebA.py
@@ -22,12 +22,45 @@ class CelebA(AbstractDataset):
     def __init__(self):
         super(CelebA, self).__init__()
         self._image_shape = CelebA.default_image_shape()
+        self._image_load_shape = (143, 143, 3)
 
     def set_image_shape(self, image_shape):
         self._image_shape = image_shape
 
     def image_shape(self):
         return (self._image_shape)
+
+    def _load_image(self, image_filename):
+        input_image = tf.io.read_file(image_filename)
+        input_image = tf.image.decode_jpeg(input_image, 3)
+        return (input_image)
+
+    def _normalize_image(self, image):
+        image = tf.cast(image, tf.float32)
+        image = tf.clip_by_value(image, 0, 255) / 127.5 - 1
+        return (image)
+
+    def _random_crop(self, image):
+        cropped_image = tf.image.random_crop(image, size=image_shape)
+        return (cropped_image)
+
+    def _random_jitter(self, image):
+        image = tf.image.resize(image,
+                                [image_load_shape[0], image_load_shape[1]])
+        image = self._random_crop(image)
+        image = tf.image.random_flip_left_right(image)
+        return (image)
+
+    def _preprocess_attributes(self, attributes_array):
+        selected_attributes = []
+        for attribute_name in default_attribute_names:
+            index = attributes_to_identifiers[attribute_name]
+            selected_attributes.append(attributes_array[index])
+
+        selected_attributes = tf.convert_to_tensor(selected_attributes)
+        selected_attributes = (selected_attributes + 1) // 2
+        selected_attributes = selected_attributes * 1.
+        return (selected_attributes)
 
     def load(self, batch_size):
         self.set_batch_size(batch_size)
@@ -44,5 +77,10 @@ class CelebA(AbstractDataset):
 
         return (train_dataset, number_of_batches)
 
-    def _augment_dataset(self, image, label):
-        return (image, label)
+    def _augment_dataset(self, image, attributes):
+        image = self._load_image(image_filename)
+        image = self._random_jitter(image)
+        image = self._normalize_image(image)
+
+        attributes = self._preprocess_attributes(attributes)
+        return (image, attributes)
