f8d8d7da37effb3dfb607acb9f7e68f88890f4f1
Mr. Pritam Prakash Shete <pritam.shete@gmail.com>
2020-08-05 15:56:42 +0530

    AttGAN training parameters are modified.

--- kgan/models/AttGAN.py
+++ kgan/models/AttGAN.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
+
 from kgan.models.WGANGP import WGANGP
 
 from kgan.models.discriminators.AttGANDiscriminator import AttGANDiscriminator
